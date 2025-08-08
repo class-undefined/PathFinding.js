e1fd83e188b68b01e55362325e5e0626acf7ae81
野漫横江 <1399226280@qq.com>
2025-08-08 11:06:43 +0800

    feat: 增加跳点识别功能并引入转向成本乘数

--- src/finders/JPFNeverMoveDiagonally.js
+++ src/finders/JPFNeverMoveDiagonally.js
@@ -9,7 +9,9 @@ var DiagonalMovement = require('../core/DiagonalMovement');
  * or vertical movements.
  */
 function JPFNeverMoveDiagonally(opt) {
+    opt = opt || {};
     JumpPointFinderBase.call(this, opt);
+    this.turnCostMultiplier = typeof opt.turnCostMultiplier === 'number' ? opt.turnCostMultiplier : 10;
 }
 
 JPFNeverMoveDiagonally.prototype = new JumpPointFinderBase();
@@ -22,7 +24,7 @@ JPFNeverMoveDiagonally.prototype.constructor = JPFNeverMoveDiagonally;
  * @return {Array<Array<number>>} The x, y coordinate of the jump point
  *     found, or null if not found
  */
-JPFNeverMoveDiagonally.prototype._jump = function(x, y, px, py) {
+JPFNeverMoveDiagonally.prototype._jump = function (x, y, px, py) {
     var grid = this.grid,
         dx = x - px, dy = y - py;
 
@@ -30,7 +32,7 @@ JPFNeverMoveDiagonally.prototype._jump = function(x, y, px, py) {
         return null;
     }
 
-    if(this.trackJumpRecursion === true) {
+    if (this.trackJumpRecursion === true) {
         grid.getNodeAt(x, y).tested = true;
     }
 
@@ -67,7 +69,7 @@ JPFNeverMoveDiagonally.prototype._jump = function(x, y, px, py) {
  * return all available neighbors.
  * @return {Array<Array<number>>} The neighbors found.
  */
-JPFNeverMoveDiagonally.prototype._findNeighbors = function(node) {
+JPFNeverMoveDiagonally.prototype._findNeighbors = function (node) {
     var parent = node.parent,
         x = node.x, y = node.y,
         grid = this.grid,
@@ -115,6 +117,67 @@ JPFNeverMoveDiagonally.prototype._findNeighbors = function(node) {
     }
 
     return neighbors;
+};
+
+JPFNeverMoveDiagonally.prototype._identifySuccessors = function (node) {
+    var grid = this.grid,
+        heuristic = this.heuristic,
+        openList = this.openList,
+        endX = this.endNode.x,
+        endY = this.endNode.y,
+        neighbors, neighbor,
+        jumpPoint, i, l,
+        x = node.x, y = node.y,
+        jx, jy, d, ng, jumpNode,
+        abs = Math.abs;
+
+    neighbors = this._findNeighbors(node);
+    for (i = 0, l = neighbors.length; i < l; ++i) {
+        neighbor = neighbors[i];
+        jumpPoint = this._jump(neighbor[0], neighbor[1], x, y);
+        if (jumpPoint) {
+            jx = jumpPoint[0];
+            jy = jumpPoint[1];
+            jumpNode = grid.getNodeAt(jx, jy);
+
+            if (jumpNode.closed) {
+                continue;
+            }
+
+            // 仅正交：使用曼哈顿距离作为从当前节点到跳点的增量代价
+            d = abs(jx - x) + abs(jy - y);
+            var incrementalCost = d * jumpNode.weight;
+
+            // 若从 parent->node 到 node->jumpPoint 方向发生变化，则乘以惩罚系数
+            if (node.parent) {
+                var prevDx = Math.sign(node.x - node.parent.x);
+                var prevDy = Math.sign(node.y - node.parent.y);
+                var currDx = Math.sign(jx - x);
+                var currDy = Math.sign(jy - y);
+                var isTurn = (prevDx !== currDx) || (prevDy !== currDy);
+                if (isTurn) {
+                    incrementalCost *= this.turnCostMultiplier;
+                }
+            }
+
+            ng = node.g + incrementalCost;
+
+            if (!jumpNode.opened || ng < jumpNode.g) {
+                jumpNode.g = ng;
+                // 仍沿用启发式（默认曼哈顿）评估到终点
+                jumpNode.h = jumpNode.h || heuristic(abs(jx - endX), abs(jy - endY));
+                jumpNode.f = jumpNode.g + jumpNode.h;
+                jumpNode.parent = node;
+
+                if (!jumpNode.opened) {
+                    openList.push(jumpNode);
+                    jumpNode.opened = true;
+                } else {
+                    openList.updateItem(jumpNode);
+                }
+            }
+        }
+    }
 };
 
 module.exports = JPFNeverMoveDiagonally;
